ff115c5fbd3a6cac3857f644c7825e12e679ded1
David Solomon <66226028+SDavid24@users.noreply.github.com>
2022-08-29 15:52:55 +0100

    This is the 3rd commit

--- app/src/main/java/com/example/gittutorial/MainActivity.kt
+++ app/src/main/java/com/example/gittutorial/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is contained in 2nd commit")
+
+        today is a good day
     }
 }
